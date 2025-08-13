a5d882daa9f7fa3317e93d01eeb966c91844ce55
Juan Alejandro Noguera Cortes <1110293979@u.icesi.edu.co>
2025-08-13 12:57:21 -0500

    subiendo

---
+++ Calculadora.java
@@ -0,0 +1,27 @@
+
+// ANALISIS
+/*
+* Descripcion: 
+* Entradas: 
+* Salidas: 
+* Ejemplo: 
+*/
+import java.util.Scanner;
+
+public class Fruver {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); 
+
+
+        System.out.println("Texto: ");
+
+
+    }
+
+
+
+
+}
+
+```
